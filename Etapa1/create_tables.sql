2bc9b0be2d4e469ca68716682709520ebb7fff46
lucenear <fariasg1988@gmail.com>
2025-10-13 23:27:48 -0300

    agregado de restriccion CHECK

--- Etapa1/create_tables.sql
+++ Etapa1/create_tables.sql
@@ -14,16 +14,15 @@ CREATE TABLE IF NOT EXISTS Legajo (
 -- Creamos tabla Empleado con restricciones y constraint
 CREATE TABLE IF NOT EXISTS Empleado (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    eliminado BOOLEAN NOT NULL DEFAULT FALSE, -- CHECK (eliminado REGEXP '^[FALSE-TRUE]^'), -- Baja lógica
+    eliminado BOOLEAN NOT NULL DEFAULT FALSE, -- Baja lógica
     nombre VARCHAR(80) NOT NULL, -- máx. 80
     apellido VARCHAR(80) NOT NULL, -- máx. 80
-    dni VARCHAR(15) NOT NULL UNIQUE, -- máx. 15
+    dni VARCHAR(15) NOT NULL UNIQUE CHECK (length(dni) >= 8 and length(dni) < 16), -- máx. 15
     email VARCHAR(120), -- máx. 120
     fechaIngreso DATE,
     area VARCHAR(50), -- máx. 50
     legajo BIGINT UNIQUE,
     CONSTRAINT fk_empleado_legajo
         FOREIGN KEY (legajo) REFERENCES Legajo(id)
-        ON UPDATE CASCADE
-        ON DELETE SET NULL
+		ON UPDATE CASCADE
 );
